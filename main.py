5c13054f3761ad8c49b699fa3cce12a32408e1a9
adiletto64 <adiletdj19@gmail.com>
2025-04-17 18:59:53 +0600

    add logging

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import logging
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.ai import get_filters_from_sentence
 from app.extractions import Product, get_product_list
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 app = FastAPI()
 
@@ -24,6 +31,6 @@ class Payload(BaseModel):
 @app.post("/get")
 async def extract_products_from_home24(data: Payload, offset: int=0, limit: int=10) -> list[Product]:
     filters = get_filters_from_sentence(data.sentence)
-    print("query: ", filters.to_query_params())
+    logger.info("query -> %s", filters.to_query_params())
     products = await get_product_list(filters)
     return products
